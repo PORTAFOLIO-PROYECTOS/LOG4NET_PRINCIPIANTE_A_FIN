19cffcd170157e5104722ffc411f8a1cfcdc0e97
Hugo <hugo.rock20@hotmail.com>
2018-07-01 15:00:51 -0500

    FileAppender

--- Log4Net/Log4Net/Program.cs
+++ Log4Net/Log4Net/Program.cs
@@ -10,11 +10,11 @@ namespace Log4Net
 {
     class Program
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log = LogHelper.GetLogger();// log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellos Word");
+            Console.WriteLine("Hello Word");
             log.Error("this is my error message");
             Console.ReadLine();
         }
