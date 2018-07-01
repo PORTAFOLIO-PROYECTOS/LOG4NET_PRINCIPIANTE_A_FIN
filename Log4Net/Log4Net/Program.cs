c9f18234c0c9ae1d56424f7f60ea2481a571cc3b
Hugo <hugo.rock20@hotmail.com>
2018-07-01 13:39:44 -0500

    log4net in console show

--- Log4Net/Log4Net/Program.cs
+++ Log4Net/Log4Net/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace Log4Net
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hellos Word");
+            log.Error("this is my error message");
+            Console.ReadLine();
         }
     }
 }
